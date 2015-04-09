6083fecb73479940026a85a04f2bac90e771571b
André Gonçalves <andreslb1@gmail.com>
2015-04-09 14:10:00 +0200

    Transform object in schema driven type. Add DataGridField support

--- collective/object/object.py
+++ collective/object/object.py
@@ -2,64 +2,65 @@ from five import grok
 
 from z3c.form import group, field
 from zope import schema
-from zope.interface import invariant, Invalid
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
+from zope.interface import invariant, Invalid, Interface, implements
+from plone.supermodel import model
 from plone.dexterity.content import Container
+
+from z3c.form.form import extends
 
 from plone.directives import dexterity, form
 from plone.app.textfield import RichText
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
 from plone.namedfile.interfaces import IImageScaleTraversable
 from plone.dexterity.browser.view import DefaultView
 
+from zope.schema.fieldproperty import FieldProperty
+
 from collective.object import MessageFactory as _
 
-
-# Interface class; used to define content-type schema.
-
-class IObject(form.Schema, IImageScaleTraversable):
-    """
-    Information about a collectible object used by museums
-    """
-
-    # If you want a schema-defined interface, delete the model.load
-    # line below and delete the matching file in the models sub-directory.
-    # If you want a model-based interface, edit
-    # models/object.xml to define the content type.
-
-    form.model("models/object.xml")
+from collective.z3cform.datagridfield import DataGridFieldFactory, IDataGridField
 
 
-# Custom content-type class; objects created for this content type will
-# be instances of this class. Use this class to add content-type specific
-# methods and properties. Put methods that are mainly useful for rendering
-# in separate view classes.
+class DimensionListField(schema.List):
+    """We need to have a unique class for the field list so that we
+    can apply a custom adapter."""
+    pass
+
+class IDimension(Interface):
+    part = schema.TextLine(title=u'Part', required=False)
+    dimension = schema.TextLine(title=u'Dimension', required=False)
+    value = schema.TextLine(title=u'Value', required=False)
+    unit = schema.TextLine(title=u'Unit', required=False)
+    precision = schema.TextLine(title=u'Precision', required=False)
+    notes = schema.TextLine(title=u'Notes', required=False)
+
+class IObject(form.Schema):
+    text = RichText(
+        title=_(u"Body text"),
+        required=False
+    )
+
+    model.fieldset('collection', label=_(u'Collection'), fields=['dimension'])
+    form.widget(dimension=DataGridFieldFactory)
+    
+    dimension = DimensionListField(title=u'Dimensions', 
+        value_type=schema.Object(title=u'Dimension', schema=IDimension), 
+        required=False)
 
 class Object(Container):
     grok.implements(IObject)
-
-    # Add your class methods and properties here
     pass
 
 
-# View class
-# The view will automatically use a similarly named template in
-# object_templates.
-# Template filenames should be all lower case.
-# The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
-# You may make this the default view for content objects
-# of this type by uncommenting the grok.name line below or by
-# changing the view class name and template filename to View / view.pt.
+class EditForm(form.EditForm):
+    extends(form.EditForm)
+    grok.context(IObject)
+    grok.require('zope2.View')
+    fields = field.Fields(IObject)
+
+    fields['dimension'].widgetFactory = DataGridFieldFactory
+
 
 class ObjectView(DefaultView):
     """ sample view class """
 
     pass
-
-    # grok.name('view')
-
-    # Add view methods here
